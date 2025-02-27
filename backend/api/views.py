a01d07adc2dc275ba17b4825d6e435227528f8e7
Archie Jarvis <archiepeterjarvis@gmail.com>
2025-02-27 18:12:55 +0000

    Implement authentication and unit test login

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+from .serializers import RegisterSerializer, LoginSerializer
+
+
+class Home(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        content = {"message": "Hello, World!"}
+        return Response(content)
+
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "User registered successfully!"},
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data["user"]
+
+        refresh = RefreshToken.for_user(user)
+        tokens = {"refresh": str(refresh), "access": str(refresh.access_token)}
+
+        return Response(
+            {"message": "Login successful", "token": str(tokens)},
+            status=status.HTTP_200_OK,
+        )
