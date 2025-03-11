a8daa351391872a2a57f8ebee2791313f813565a
Archie Jarvis <archiepeterjarvis@gmail.com>
2025-03-11 01:00:41 +0000

    Setup community views and unit test them

--- backend/api/views/__init__.py
+++ backend/api/views/__init__.py
@@ -1,4 +1,5 @@
-from .auth_views import RegisterView, LoginView as RegisterView, LoginView
+from .auth_views import RegisterView as RegisterView
+from .auth_views import LoginView as LoginView
 from .home_views import HomeView as HomeView
 from .community_views import CommunityListView as CommunityListView
 from .community_views import CommunityDetailView as CommunityDetailView
