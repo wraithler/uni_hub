5834f9493757b0fc3166a4637031cd903d3e1019
s2-baysal <setenay2.baysal@live.uwe.ac.uk>
2025-03-13 23:11:05 +0000

    serializer, view, pagination and couple other bits for events

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,7 +10,13 @@ from api.views import (
     CommunityCreateView,
     CommunityCategoryListView,
     CommunityCategoryCreateView,
-    CommunityCategoryDetailView, UserDetailView,
+    CommunityCategoryDetailView,
+    UserDetailView,
+    EventListView,
+    EventDetailView,
+    EventCreateView,
+    EventAttendanceView,
+    VirtualEventAccessView,
 )
 
 urlpatterns = [
@@ -20,28 +26,17 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("communities/", CommunityListView.as_view(), name="community-list"),
-    path(
-        "communities/<int:pk>/", CommunityDetailView.as_view(), name="community-detail"
-    ),
+    path("communities/<int:pk>/", CommunityDetailView.as_view(), name="community-detail"),
     path("communities/create/", CommunityCreateView.as_view(), name="community-create"),
-    path(
-        "community-categories/",
-        CommunityCategoryListView.as_view(),
-        name="community-category-list",
-    ),
-    path(
-        "community-categories/<int:pk>/",
-        CommunityCategoryDetailView.as_view(),
-        name="community-category-detail",
-    ),
-    path(
-        "community-categories/create/",
-        CommunityCategoryCreateView.as_view(),
-        name="community-category-create",
-    ),
-    path(
-        "user/",
-        UserDetailView.as_view(),
-        name="user-detail",
-    )
+    path("community-categories/", CommunityCategoryListView.as_view(), name="community-category-list"),
+    path("community-categories/<int:pk>/", CommunityCategoryDetailView.as_view(), name="community-category-detail"),
+    path("community-categories/create/", CommunityCategoryCreateView.as_view(), name="community-category-create"),
+    path("user/", UserDetailView.as_view(), name="user-detail"),
+    
+    # Event URLs
+    path("events/", EventListView.as_view(), name="event-list"),
+    path("events/<int:pk>/", EventDetailView.as_view(), name="event-detail"),
+    path("events/create/", EventCreateView.as_view(), name="event-create"),
+    path("events/<int:pk>/attend/", EventAttendanceView.as_view(), name="event-attendance"),
+    path("events/<int:pk>/virtual/", VirtualEventAccessView.as_view(), name="virtual-event-access"),
 ]
