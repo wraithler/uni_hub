230ffad28cefcc9dbf353bb9270feac992d80eeb
Archie Jarvis <archiepeterjarvis@gmail.com>
2025-03-13 22:17:47 +0000

    Setup email verification and the email container in docker

--- backend/api/models.py
+++ backend/api/models.py
@@ -15,6 +15,7 @@ class User(AbstractUser):
     year_of_study = models.IntegerField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    email_verified = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
