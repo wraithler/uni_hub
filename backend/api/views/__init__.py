002940da41fd1da65a11872403e841f513d68fa3
Beyzanrkyc <beyzanurkyc.bk@gmail.com>
2025-03-14 01:33:56 +0000

    add lines in init file of views for posts and comments

--- backend/api/views/__init__.py
+++ backend/api/views/__init__.py
@@ -8,3 +8,7 @@ from .community_views import CommunityCategoryListView as CommunityCategoryListV
 from .community_views import CommunityCategoryDetailView as CommunityCategoryDetailView
 from .community_views import CommunityCategoryCreateView as CommunityCategoryCreateView
 from .user_views import UserDetailView as UserDetailView
+from .posts_views import PostListView as PostListView
+from .posts_views import PostDetailView as PostDetailView
+from .posts_views import PostCreateView as PostCreateView
+from .posts_views import CommentCreateView as CommentCreateView
