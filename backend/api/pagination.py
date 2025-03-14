7597453f2fb629675632988e8341c99e7bd79603
Beyzanrkyc <beyzanurkyc.bk@gmail.com>
2025-03-14 01:32:00 +0000

    Add necessary pagination parts for posts and comments

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -11,3 +11,14 @@ class CommunityCategoryPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 100
+
+class PostPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+
+class CommentPagination(PageNumberPagination):
+    page_size = 20  
+    page_size_query_param = "page_size"
+    max_page_size = 200
