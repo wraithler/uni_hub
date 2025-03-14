fd8c3330c36c52364cc68ec56e2773f9fb555abd
Beyzanrkyc <beyzanurkyc.bk@gmail.com>
2025-03-14 01:33:22 +0000

    add lines in init file of serializers for posts and comments

--- backend/api/serializers/__init__.py
+++ backend/api/serializers/__init__.py
@@ -5,3 +5,5 @@ from .community_serializer import (
     CommunityCategorySerializer as CommunityCategorySerializer,
 )
 from .user_serializer import UserSerializer as UserSerializer
+from .posts_serializer import CommentSerializer as CommentSerializer
+from .posts_serializer import PostSerializer as PostSerializer
