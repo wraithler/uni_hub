26aaa244fe2d1340fa17206ed65c036c9f077629
Archie Jarvis <archiepeterjarvis@gmail.com>
2025-03-16 17:45:42 +0000

    Start working on websockets for messaging system

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -9,8 +9,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+from core import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    )
+})
