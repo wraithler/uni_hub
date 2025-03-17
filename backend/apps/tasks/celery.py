d164c8f461a37868a158b9ac551870ebeeaefae9
Archie Jarvis <archiepeterjarvis@gmail.com>
2025-03-17 18:33:42 +0000

    Setup email app with shared_task

--- backend/apps/tasks/celery.py
+++ backend/apps/tasks/celery.py
@@ -3,9 +3,10 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.django.base")
 
-app = Celery("apps")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
+celery_app = Celery("uni_hub")
+celery_app.config_from_object(settings, namespace="CELERY")
+celery_app.autodiscover_tasks(["apps.tasks", "apps.emails"])
