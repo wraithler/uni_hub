89800f03d67a9bc6c0daee86426cca449dd4a352
Beyzanrkyc <beyzanurkyc.bk@gmail.com>
2025-03-20 18:31:59 +0000

    fix views for post

--- backend/api/views/posts_views.py
+++ backend/api/views/posts_views.py
@@ -3,20 +3,20 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from api.mixins import SearchMixin
-from api.models import Post, Comment
-from api.pagination import CommunityPagination
-from api.serializers import PostSerializer, CommentSerializer
+from api.models import Post
+from api.pagination import PostPagination
+from api.serializers import PostSerializer
 
 
 class PostListView(SearchMixin):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    pagination_class = CommunityPagination
+    pagination_class = PostPagination
 
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
-    search_fields = ["content", "user__name", "community__name"]
+    search_fields = ["content", "created_by__username", "community__name"]
 
 
 class PostDetailView(RetrieveAPIView):
@@ -34,10 +34,3 @@ class PostCreateView(CreateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
-
-class CommentCreateView(CreateAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
