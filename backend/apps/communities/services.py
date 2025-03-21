f3075195d4f6861e0bfa1dacce0542e8ec8f6511
Archie Jarvis <archiepeterjarvis@gmail.com>
2025-03-21 18:53:16 +0000

    Setup invitations for private communities

--- backend/apps/communities/services.py
+++ backend/apps/communities/services.py
@@ -3,7 +3,8 @@ from typing import List
 from django.db import transaction
 
 from apps.common.services import model_update
-from apps.communities.models import CommunityCategory, Community
+from apps.communities.models import CommunityCategory, Community, CommunityInvitation
+from apps.core.exceptions import ApplicationError
 from apps.users.models import BaseUser
 
 
@@ -18,7 +19,7 @@ def community_category_create(*, name: str, description: str) -> CommunityCatego
 def community_category_update(
     *, community_category: CommunityCategory, data
 ) -> CommunityCategory:
-    non_side_effect_fields: List[str] = []
+    non_side_effect_fields: List[str] = ["name", "description"]
 
     community_category, has_updated = model_update(
         instance=community_category, fields=non_side_effect_fields, data=data
@@ -33,7 +34,7 @@ def community_create(
     name: str,
     description: str,
     category: CommunityCategory,
-    created_by: BaseUser = None,
+    created_by: BaseUser,
     emoji: str = None,
 ) -> Community:
     community = Community.objects.create(
@@ -44,15 +45,57 @@ def community_create(
         emoji=emoji,
     )
 
+    community.memberships.create(user=created_by)
+
     return community
 
 
 @transaction.atomic
 def community_update(*, community: Community, data) -> Community:
-    non_side_effect_fields: List[str] = []
+    non_side_effect_fields: List[str] = ["name", "description", "emoji"]
 
     community, has_updated = model_update(
         instance=community, fields=non_side_effect_fields, data=data
     )
 
     return community
+
+
+@transaction.atomic
+def community_join(*, community: Community, user: BaseUser) -> Community:
+    if community.is_member(user):
+        raise ApplicationError("User is already a member of this community")
+
+    if community.is_private:
+        invitation = community.invitations.filter(user=user).first()
+
+        if invitation:
+            community_invitation_update(
+                invitation=invitation, data={"is_accepted": True}
+            )
+        else:
+            raise ApplicationError("User is not invited to this community")
+
+    community.memberships.create(user=user)
+
+    return community
+
+
+@transaction.atomic
+def community_invitation_create(*, community: Community, user: BaseUser) -> CommunityInvitation:
+    invitation = CommunityInvitation.objects.create(community=community, user=user)
+
+    return invitation
+
+
+@transaction.atomic
+def community_invitation_update(
+    *, invitation: CommunityInvitation, data
+) -> CommunityInvitation:
+    non_side_effect_fields: List[str] = ["is_accepted"]
+
+    invitation, has_updated = model_update(
+        instance=invitation, fields=non_side_effect_fields, data=data
+    )
+
+    return invitation
