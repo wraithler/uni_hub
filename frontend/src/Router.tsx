6a598b0ef8c181b4e124f6d0ce3d5919bde8baa5
Archie Jarvis <archiepeterjarvis@gmail.com>
2025-03-24 15:39:45 +0000

    Start setting up community page

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -6,6 +6,7 @@ import { NotFoundPage } from '@/pages/not_found/NotFound.page';
 import { VerifyEmailPage } from '@/pages/VerifyEmail.page';
 import { AuthPage } from './pages/Auth.page';
 import { HomePage } from './pages/Home.page';
+import {CommunityPage} from "@/pages/communities/Community.page";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,14 @@ const router = createBrowserRouter([
         <CommunitiesPage />
       </ProtectedRoute>
     ),
+  },
+  {
+    path: '/community/:communityId',
+    element: (
+        <ProtectedRoute>
+          <CommunityPage/>
+        </ProtectedRoute>
+    )
   },
   {
     path: '/chat/:chatId',
