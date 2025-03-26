75ee672b5834cb3b329e2dfec37e4f2761ad4b0d
Archie Jarvis <archiepeterjarvis@gmail.com>
2025-03-26 18:36:10 +0000

    Setup tags & categories correctly

--- backend/apps/communities/models.py
+++ backend/apps/communities/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from apps.common.models import BaseModel
 
 
-class CommunityTags(BaseModel):
+class CommunityTag(BaseModel):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
 
@@ -29,7 +29,7 @@ class Community(BaseModel):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
-    tags = models.ManyToManyField(CommunityTags, related_name="communities")
+    tags = models.ManyToManyField(CommunityTag, related_name="communities")
     category = models.ForeignKey(
         CommunityCategory, on_delete=models.CASCADE, related_name="communities"
     )
