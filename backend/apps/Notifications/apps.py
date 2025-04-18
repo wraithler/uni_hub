cf2f648be30463fe6b08fe9519a6b1aa615ede1a
Kay288 <khalidalfadala@gmail.com>
2025-04-18 21:01:12 +0100

    name changed to apps.notifications

--- backend/apps/Notifications/apps.py
+++ backend/apps/Notifications/apps.py
@@ -1,11 +1,6 @@
 from django.apps import AppConfig
 
-
 class NotificationsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.notifications"
     verbose_name = "Notifications"
-
-    def ready(self):
-        
-        import apps.notifications.signals  
