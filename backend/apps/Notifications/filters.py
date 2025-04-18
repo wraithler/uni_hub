8dca9dfd271def7d4da129396201713a702ac782
Kay288 <khalidalfadala@gmail.com>
2025-04-18 21:06:18 +0100

    code refactored

--- backend/apps/Notifications/filters.py
+++ backend/apps/Notifications/filters.py
@@ -1,22 +1,13 @@
 import django_filters
 from django_filters import DateFromToRangeFilter, BooleanFilter, CharFilter
-
 from apps.notifications.models import Notification
 
-
 class NotificationFilter(django_filters.FilterSet):
-    
     created_at = DateFromToRangeFilter()
-    
-    
     is_read = BooleanFilter()
-    
-    
     notification_type = CharFilter(lookup_expr='icontains')
+    recipient__email = CharFilter(field_name='recipient__email', lookup_expr='icontains')
     
-    
-    recipient__username = CharFilter(field_name='recipient__username', lookup_expr='icontains')
-
     class Meta:
         model = Notification
         fields = (
@@ -24,6 +15,6 @@ class NotificationFilter(django_filters.FilterSet):
             "is_read",
             "notification_type",
             "recipient",
-            "recipient__username",
+            "recipient__email",
             "created_at",
         )
