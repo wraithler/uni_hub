0781834f6fe767db8e83956bdc3adf482f874eef
Archie Jarvis <archiepeterjarvis@gmail.com>
2025-04-21 20:53:43 +0100

    Add created by to post type

--- frontend/src/api/posts/postTypes.ts
+++ frontend/src/api/posts/postTypes.ts
@@ -1,12 +1,13 @@
-import {Community} from "@/api/communities/communityTypes.ts";
+import { Community } from "@/api/communities/communityTypes.ts";
+import { UserMe } from "@/api/old/types/users.ts";
 
 type Post = {
-    id?: number;
-    content: string;
-    community?: Community | number;
+  id?: number;
+  content: string;
+  community?: Community | number;
+  created_by: UserMe; // TODO: Change from UserMe to generic
 
-    // TODO: Add likes/comments
-    // created_by:
-}
+  // TODO: Add likes/comments
+};
 
 export type { Post };
