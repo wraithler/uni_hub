53e9b2285fc7b8aec1bf2f1b405a2643d0343a3a
Archie Jarvis <archiepeterjarvis@gmail.com>
2025-04-21 22:15:06 +0100

    Remove command menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { Router } from "@/components/core/Router.tsx";
 import { EmailVerificationToast } from "@/components/auth/EmailVerificationToast.tsx";
 import { Toaster } from "@/components/ui/sonner.tsx";
 import { AuthProvider } from "@/components/auth/AuthProvider.tsx";
-import { CommandMenu } from "@/components/core/CommandMenu.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -15,7 +14,6 @@ function App() {
       </QueryClientProvider>
       <Toaster />
       <EmailVerificationToast />
-      <CommandMenu />
     </AuthProvider>
   );
 }
