2dcea219abac2d0ac26b66efb2918fc7067eadf9
Archie Jarvis <archiepeterjarvis@gmail.com>
2025-04-22 20:13:07 +0100

    Add user drop down menu

---
+++ frontend/src/components/users/UserDropDownMenu.tsx
@@ -0,0 +1,53 @@
+import { useAuth } from "../auth/AuthProvider";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import {LogOut, MessageSquare, Settings, UserCircle, UserPen} from "lucide-react";
+import {useNavigate} from "react-router-dom";
+
+export default function UserDropdownMenu() {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost">
+          <UserCircle className="w-4 h-4" />
+          <span>{user?.first_name} {user?.last_name}</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem onClick={() => navigate("/profile")}>
+            <UserPen/>
+            Profile
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => navigate("/settings")}>
+            <Settings/>
+            Settings
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => navigate("/support")}>
+          <MessageSquare/>
+          Support
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={logout}>
+          <LogOut/>
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
