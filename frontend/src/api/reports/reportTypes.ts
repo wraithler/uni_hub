788824e4fde45eb7f2380193856cedc1ade93da3
Beyzanrkyc <beyzanurkyc.bk@gmail.com>
2025-04-26 21:58:22 +0100

    add reportTypes for reports

---
+++ frontend/src/api/reports/reportTypes.ts
@@ -0,0 +1,83 @@
+import { PaginationResponse } from "@/api";
+import { User } from "@/api/users/userTypes.ts";
+import { Community } from "../communities/communityTypes";
+
+type ReportCategory = {
+  id: number;
+  name: string;
+};
+
+type ReportStatus = 'PENDING' | 'UNDER_REVIEW' | 'RESOLVED' | 'CLOSED' | 'REJECTED';
+
+type Report = {
+  // basic parameters
+  id?: number;
+  title: string;
+  description: string;
+  category: number;
+  evidence_links: any;
+  status: ReportStatus;
+  
+  // users
+  reported_by: User;
+  reported_user: number;
+  community: Community;
+  
+  // resolution
+  reviewed_by: number;
+  resolution_notes: string;
+  
+  // timestamps
+  created_at: string;
+  updated_at: string;
+};
+
+type ReportList = PaginationResponse & {
+  // extend PaginationResponse to have results of type Report[]
+  results: Report[];
+};
+
+type ReportAttachment = {
+  id: number;
+  file: string;
+  description: string | null;
+};
+
+// Input types
+type ReportCreateInput = {
+  title: string;
+  description: string;
+  category: number;
+  reported_user?: User;
+  community?: Community;
+  evidence_links?: any | null;
+};
+
+type ReportUpdateInput = {
+  title?: string;
+  description?: string;
+  evidence_links?: any;
+};
+
+type ReportResolveInput = {
+  status: ReportStatus;
+  resolution_notes?: string | null;
+};
+
+type ReportAttachmentInput = {
+  report: number;
+  file: File;
+  description?: string | null;
+};
+
+export type { 
+  ReportCategory, 
+  ReportStatus, 
+  Report, 
+  ReportList, 
+  ReportAttachment,
+  ReportCreateInput,
+  ReportUpdateInput,
+  ReportResolveInput,
+  ReportAttachmentInput
+};
