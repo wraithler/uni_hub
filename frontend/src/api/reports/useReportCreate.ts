f9bb237dede9d787d2e8436b16ae7a61cd3e68b9
Beyzanrkyc <beyzanurkyc.bk@gmail.com>
2025-04-27 21:58:31 +0100

    add useReportCreate for reports

--- frontend/src/api/reports/useReportCreate.ts
+++ frontend/src/api/reports/useReportCreate.ts
@@ -4,38 +4,39 @@ import { reportQueryKeys } from "@/api/reports/reportQueryKeys.ts";
 import { toast } from "sonner";
 import { useNavigate } from "react-router-dom";
 import { TSFix } from "@/api";
-import { Report, ReportCreateInput } from "@/api/reports/reportTypes.ts";
+import { Report } from "@/api/reports/reportTypes.ts";
 
 export function useReportCreate() {
   const queryClient = useQueryClient();
-//   const toast = useToast();
   const navigate = useNavigate();
-
-  const createReportFn = async (newReport: ReportCreateInput) => {
+  
+  const createReportFn = async (newReport: Report) => {
     const response = await api.post("/reports/create/", newReport);
     return response.data;
   };
-
+  
   return useMutation({
     mutationFn: createReportFn,
-    onSuccess: async () => {
+    onMutate: async () => {
       await queryClient.cancelQueries({ queryKey: reportQueryKeys.all });
     },
-    onSettled: (data: Report) => {
+    onSuccess: (data: Report) => {
       toast.success("Your report was created successfully!", {
-        label: "View Report",
-        onClick: () => {
-          navigate(`/reports/${data.id}`);
+        action: {
+          label: "View Report",
+          onClick: () => {
+            navigate(`/reports/${data.id}`);
+          },
         },
       });
     },
-    onError: (_err, _newReport, context?: TSFix) => {
+    onError: (_err, _newReport, context: TSFix) => {
       queryClient.setQueryData(
         reportQueryKeys.all,
         context.previousReports
       );
     },
-    onMutate: () => {
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: reportQueryKeys.all });
     }
   });
