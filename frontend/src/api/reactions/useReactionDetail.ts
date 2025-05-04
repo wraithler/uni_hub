c551172e94ee6cf4895229f16e01bd92e16713d9
Beyzanrkyc <beyzanurkyc.bk@gmail.com>
2025-05-05 00:25:54 +0100

    implement hook to fetch detailed reaction data

---
+++ frontend/src/api/reactions/useReactionDetail.ts
@@ -0,0 +1,58 @@
+import api from "@/api/apiClient.ts";
+import { useQuery } from "@tanstack/react-query";
+import { commentQueryKeys, likeQueryKeys } from "@/api/reactions/reactionQueryKeys.ts";
+import { Comment, Like } from "./reactionTypes";
+
+type UseCommentDetailProps = {
+  id: number;
+};
+
+export function useCommentDetail({ id }: UseCommentDetailProps) {
+  const getCommentFn = async () => {
+    const response = await api.get(`/comments/${id}/`);
+    return response.data as Comment;
+  };
+
+  return useQuery({
+    queryKey: commentQueryKeys.detail(id),
+    queryFn: getCommentFn,
+  });
+}
+
+type UseLikeDetailProps = {
+  id: number;
+};
+
+export function useLikeDetail({ id }: UseLikeDetailProps) {
+  const getLikeFn = async () => {
+    const response = await api.get(`/likes/${id}/`);
+    return response.data as Like;
+  };
+
+  return useQuery({
+    queryKey: likeQueryKeys.detail(id),
+    queryFn: getLikeFn,
+  });
+}
+
+type UseHasLikedProps = {
+  content_type: string;
+  object_id: number;
+};
+
+export function useHasLiked({ content_type, object_id }: UseHasLikedProps) {
+  const getHasLikedFn = async () => {
+    const response = await api.get(`/likes/check/`, {
+      params: {
+        content_type,
+        object_id,
+      },
+    });
+    return response.data;
+  };
+
+  return useQuery({
+    queryKey: likeQueryKeys.forContent({ content_type, object_id }),
+    queryFn: getHasLikedFn,
+  });
+}
